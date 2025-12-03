ca86ac602c2ae1060314bf2d812fcce02e00d52f
Emma Svensson <emsv2000@student.miun.se>
2025-12-03 11:41:35 +0100

    Uppgift 8

---
+++ exempeltenta-bil-blackjack/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exempeltenta_bil_blackjack
+{
+    public class Car
+    {
+        public string LicencePlate { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+    }
+}
+//Uppgift 8 klar
---
+++ exempeltenta-bil-blackjack/CarRegister.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exempeltenta_bil_blackjack
+{
+    public class CarRegister
+    {
+        public List<Car> Cars { get; set; }
+
+        public CarRegister()
+        {
+            Cars = new();
+        }
+    }
+}
