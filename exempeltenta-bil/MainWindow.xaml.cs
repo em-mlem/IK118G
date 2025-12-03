8dbe6f7cb23c8ce15a125b27f71d2390f9a187ac
Emma Svensson <emsv2000@student.miun.se>
2025-12-03 10:43:59 +0100

    Uppgift 2 klar

--- exempeltenta-bil/MainWindow.xaml.cs
+++ exempeltenta-bil/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics.Eventing.Reader;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,11 +17,22 @@ namespace exempeltenta_bil
     /// </summary>
     public partial class MainWindow : Window
     {
-        int b = 3500;
-        int bPlus = 4250;
-        int carweight;
-        string lblweight;
+        public bool KontrolleraRegnummer(string regNr)
+        {
+            if (regNr.Length != 7)
+                return false;
 
+            if (!char.IsLetter(regNr[0]) || !char.IsLetter(regNr[1]) || !char.IsLetter(regNr[2]))
+                return false;
+
+            if (regNr[3] != ' ')
+                return false;
+
+            if (!char.IsDigit(regNr[4]) || !char.IsDigit(regNr[5]) || !char.IsDigit(regNr[6]))
+                return false;
+
+            return true;
+        }
         public MainWindow()
         {
             InitializeComponent();
@@ -28,6 +40,8 @@ namespace exempeltenta_bil
 
         private void calculateWeight_Click(object sender, RoutedEventArgs e)
         {
+            int b = 3500;
+            int bPlus = 4250;
             string lblweight = totalVikt.Text;
             int carweight = int.Parse(totalVikt.Text);
             int resultb = b - carweight;
@@ -36,6 +50,21 @@ namespace exempeltenta_bil
             resultB.Content = $"{resultb} kg";
             resultBPlus.Content = $"{resultbplus} kg";
             // Uppgift 1 klar
+        }
+
+        private void regCtrl_Click(object sender, RoutedEventArgs e)
+        {
+            string regInput = regNr.Text;
+
+            if (KontrolleraRegnummer(regInput))
+            {
+                return;
+            }
+            else
+            {
+                MessageBox.Show("Skylten har ett felaktigt antal tecken");
+            }
+            //Uppgift 2 klar
         }
     }
 }
