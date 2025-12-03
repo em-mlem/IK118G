c1b11e11830c3fb12b0d9c48c3473c06a3a8514a
Emma Svensson <emsv2000@student.miun.se>
2025-12-03 11:23:59 +0100

    Uppgift 5 och 6 klar

---
+++ exempeltenta-bil-blackjack/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace exempeltenta_bil_blackjack
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int[] SlumpaRegNr()
+        {
+            Random r = new();
+            int[] resultat = new int[3];
+
+            for (int i = 0; i < resultat.Length; i++)
+            {
+                resultat[i] = r.Next(0, 10);
+            }
+            return resultat;
+        }
+
+        private int total1 = 0;
+        private int total2 = 0;
+        private int total3 = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnbet_Click(object sender, RoutedEventArgs e)
+        {
+            int[] kod = SlumpaRegNr();
+
+            lblCode1.Content = kod[0];
+            lblCode2.Content = kod[1];
+            lblCode3.Content = kod[2];
+            //Uppgift 5 klar
+            
+            int result1 = int.Parse(lblCode1.Content.ToString());
+            int result2 = int.Parse(lblCode2.Content.ToString());
+            int result3 = int.Parse(lblCode3.Content.ToString());
+
+            total1 += result1;
+            total2 += result2;
+            total3 += result3;
+
+            lblTotal1.Content = total1;
+            lblTotal2.Content = total2;
+            lblTotal3.Content = total3;
+            //Uppgift 6 klar
+        }
+    }
+}
