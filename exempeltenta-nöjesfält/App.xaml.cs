3a599dde3b028332bc5aeaea25d21f4c3846f9b9
Emma Svensson <emsv2000@student.miun.se>
2025-12-08 20:27:40 +0100

    Nöjesfält 1

---
+++ exempeltenta-nöjesfält/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace exempeltenta_nöjesfält
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ exempeltenta-nöjesfält/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace exempeltenta_nöjesfält
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            string peoplebalance = txtpeople.Text;
+            int people = int.Parse(peoplebalance);
+
+            if (people % 2 == 0)
+            {
+                MessageBox.Show("Karusellen är i balans");
+            }
+            else
+            {
+                MessageBox.Show("Karusellen är i obalans");
+            }
+        }
+    }
+}
