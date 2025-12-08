066cafbb1a677cd67f1eac4eacf738f59c1288d1
Emma Svensson <emsv2000@student.miun.se>
2025-12-08 20:42:21 +0100

    Uppgift 2 och 3

--- exempeltenta-nöjesfält/MainWindow.xaml.cs
+++ exempeltenta-nöjesfält/MainWindow.xaml.cs
@@ -34,5 +34,40 @@ namespace exempeltenta_nöjesfält
                 MessageBox.Show("Karusellen är i obalans");
             }
         }
+
+        private void btnqueue_Click(object sender, RoutedEventArgs e)
+        {
+            string peoplequeue = txtpeople.Text;
+            int CanRideOnNextRound = int.Parse(peoplequeue);
+
+            if(CanRideOnNextRound>=48)
+            {
+                MessageBox.Show("Du får vänta en runda till innan du kan åka");
+            }
+            else
+            {
+                MessageBox.Show("Nu är det din tur att åka!");
+            }
+
+        }
+
+        private void btnwait_Click(object sender, RoutedEventArgs e)
+        {
+            string peoplewait = txtpeople.Text;
+            int queue = int.Parse(peoplewait);
+            int CalculateWaitingTime;
+
+            if (queue>=48)
+            {
+                CalculateWaitingTime = 5;
+                MessageBox.Show($"Det är {queue} före dig i kön, det är {CalculateWaitingTime} minuters väntetid");
+            }
+            else if (queue>=102)
+            { 
+                CalculateWaitingTime = 10;
+                MessageBox.Show($"Det är {queue} före dig i kön, det är {CalculateWaitingTime} minuters väntetid");
+            }
+
+        }
     }
 }
